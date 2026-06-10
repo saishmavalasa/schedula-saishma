0f7637c551106b795a87dd3d06441c28cef130ba
Saishma <saishmavalsa@gmail.com>
2026-06-10 17:42:59 +0530

    Day 5: configure backend for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,17 +14,22 @@ import { PatientModule } from './patient/patient.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5433'),
+      port: parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: false,
       dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
+
     AuthModule,
     DoctorModule,
     PatientModule,
